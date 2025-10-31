171aeb337144e40b15792f71ba52192390d50c49
Jérémie Bardon <jbardon@petalmd.com>
2025-10-31 13:33:36 -0400

    SEME-4839: Add input

--- step.sh
+++ step.sh
@@ -4,6 +4,7 @@ set -e
 LICENSE_FILE="ixguard-license.txt"
 PROTECTED_ARCHIVE="protected.xcarchive"
 IXGUARD_GENERATED_FILES_DIRECTORY="ixguard-files"
+CONFIG_VERSION="production@latest"
 
 # Download licenses
 if [ -n "${BITRISEIO_IXGUARD_LICENSE_URL}" ]; then
@@ -13,23 +14,18 @@ else
     exit 1
 fi
 
-# Make sure ixguard is installed
-if ! command -v ixguard >/dev/null 2>&1; then
-    echo "iXGuard must first be installed."
-    exit 1
+# Set config version
+if [ -n "${BITRISEIO_IXGUARD_CONFIG_VERSION}" ]; then
+    CONFIG_VERSION="${BITRISEIO_IXGUARD_CONFIG_VERSION}"
 fi
 
 # Process the unprotected xcarchive
-ixguard -config "ixguard.yml" -o="$PROTECTED_ARCHIVE" "$BITRISE_XCARCHIVE_PATH"
+guardsquare protect --ssh-agent --no-browser -o "$IXGUARD_GENERATED_FILES_DIRECTORY/$PROTECTED_ARCHIVE" --force-license-sync --config "$CONFIG_VERSION" --out-dir "$IXGUARD_GENERATED_FILES_DIRECTORY" "$BITRISE_XCARCHIVE_PATH"
 
 # Make the protected archive available from outside this Step
 envman add --key PROTECTED_ARCHIVE --value "$(realpath $PROTECTED_ARCHIVE)"
 
-# Group and compress the generated files
-mkdir "$IXGUARD_GENERATED_FILES_DIRECTORY"
-mv "ixguard-dsymutil.log" "ixguard.log" "mapping.yml" "protectionreport.html" "statistics.yml" "support_files.zip" "telemetry_dump.json" "$IXGUARD_GENERATED_FILES_DIRECTORY/"
-cp -r "$PROTECTED_ARCHIVE" "$IXGUARD_GENERATED_FILES_DIRECTORY/"
 zip -r "$IXGUARD_GENERATED_FILES_DIRECTORY.zip" "$IXGUARD_GENERATED_FILES_DIRECTORY"
-
 mv "$IXGUARD_GENERATED_FILES_DIRECTORY.zip" "$BITRISE_DEPLOY_DIR/"
+
 exit 0
