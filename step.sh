1eefcedfb86d531b79e2595f0efdeba4ecb5826a
Jérémie Bardon <jbardon@petalmd.com>
2025-11-07 17:16:25 -0500

    SEME-4839: No need for License

--- step.sh
+++ step.sh
@@ -1,23 +1,9 @@
 #!/bin/bash
 set -e
 
-LICENSE_FILE="ixguard-license.txt"
 PROTECTED_ARCHIVE="protected.xcarchive"
 IXGUARD_GENERATED_FILES_DIRECTORY="ixguard-files"
-CONFIG_VERSION="production@latest"
-
-# Download licenses
-if [ -n "${BITRISEIO_IXGUARD_LICENSE_URL}" ]; then
-    curl ${BITRISEIO_IXGUARD_LICENSE_URL} -o "$LICENSE_FILE"
-else
-    echo "Need to import the ixguard license file into Bitrise."
-    exit 1
-fi
-
-# Set config version
-if [ -n "${BITRISEIO_IXGUARD_CONFIG_VERSION}" ]; then
-    CONFIG_VERSION="${BITRISEIO_IXGUARD_CONFIG_VERSION}"
-fi
+CONFIG_VERSION=${config_version}
 
 # Process the unprotected xcarchive
 guardsquare protect --ssh-agent --no-browser -o "$IXGUARD_GENERATED_FILES_DIRECTORY/$PROTECTED_ARCHIVE" --force-license-sync --config "$CONFIG_VERSION" --out-dir "$IXGUARD_GENERATED_FILES_DIRECTORY" "$BITRISE_XCARCHIVE_PATH"
